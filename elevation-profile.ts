02f77a731a2eac4cadd10d929fff0b4d2d0b889f
Frederic Junod <frederic.junod@camptocamp.com>
2024-02-06 10:30:52 +0100

    fix: register custom events into GlobalEventHandlersEventMap

--- elevation-profile.ts
+++ elevation-profile.ts
@@ -215,7 +215,7 @@ export default class ElevationProfile extends LitElement {
       x: 0,
       y: 0,
     };
-    this.dispatchEvent(new CustomEvent('out'));
+    this.dispatchEvent(new CustomEvent<void>('out'));
   }
 
   override createRenderRoot() {
@@ -227,5 +227,9 @@ export default class ElevationProfile extends LitElement {
 declare global {
   interface HTMLElementTagNameMap {
     'elevation-profile': ElevationProfile;
+  }
+  interface GlobalEventHandlersEventMap {
+    over: CustomEvent<OverDetails>;
+    out: CustomEvent<void>;
   }
 }
